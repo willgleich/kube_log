5dbbed3953b403e66cc84f69232afbdb0afb1077
Will <22464726+willgleich@users.noreply.github.com>
2020-08-29 12:52:07 -0600

    Adding in line parse functionality Reset Author

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, SubmitField, TextField
 import kube_log
 
 app = Flask(__name__)
@@ -9,8 +9,8 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 class LogForm(FlaskForm):
     choice = kube_log.get_pods_for_selectfield('metallb-system')
-    pod = SelectField('Kubernetes pod', choices=choice)
-    lines = TextField()
+    pod = SelectField(u'Kubernetes pod', choices=choice)
+    lines = TextField(u'Lines', default=1000)
     submit = SubmitField()
 
 
@@ -18,7 +18,7 @@ class LogForm(FlaskForm):
 def kube_log_view():
     form = LogForm()
     if form.validate_on_submit():
-        return render_template('kube_log.html', form=form, data=kube_log.get_pod_logs(form.data['pod'], 'metallb-system'))
+        return render_template('kube_log.html', form=form, data=kube_log.get_pod_logs(form.data['pod'], 'metallb-system', form.data['lines']))
     return render_template('kube_log.html', form=form)
 
 
--- kube_log.py
+++ kube_log.py
@@ -1,6 +1,6 @@
 from kubernetes import client, config
 ##If using a nonstanrd config
-#config.load_kube_config()
+config.load_kube_config()
 
 
 def get_pods_for_selectfield(namespace):
@@ -23,8 +23,10 @@ def get_pod_logs(podname, namespace, lines):
     function to get the last n lines of a pod in a specific namesapce
     '''
     v1 = client.CoreV1Api()
-    ret = v1.read_namespaced_pod_log(podname, namespace)[:-lines]
+    ret = v1.read_namespaced_pod_log(podname, namespace)
+    ret = '\n'.join(ret.split('\n')[-(int(lines)+1):])
     return ret
 
 if __name__ == '__main__':
+    print(get_pod_logs('speaker-77zkr', 'metallb-system', 10))
     pass
